adf25ceaff56143722b0052ec75634589142823f
Julien <julien.artachonieto@gmail.com>
2022-02-03 17:25:33 +0100

    Ajout d'une route post /poucavius/gaulois pour ajouter une entrée dans la base

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const connection = require("./db-config");
 const app =express();
 
-const port = process.env.PORT ?? 3200;
+const port = process.env.PORT ?? 3100;
 
 app.use(express.json());
 
@@ -13,6 +13,29 @@ connection.connect((err) => {
         console.log("connected as id " + connection.threadId);
     }
 });
+app.post ("/poucavius/gaulois",(req,res)=>{
+    const {name, age,tall,hair_color,sex,pilosity,armed,localisation,stature,status}=req.body
+    connection.query("INSERT INTO gaulois (name, age,tall,hair_color,sex,pilosity,armed,localisation,stature,status) VALUES(?,?,?,?,?,?,?,?,?,?)",
+        [name, age,tall,hair_color,sex,pilosity,armed,localisation,stature,status],(err,result)=>{
+        if (err){
+            console.log(err);
+            res.status(500).send("Error saving Gaulois !")
+        }else{
+            res.status(200).send("Gaulois sucessfullly poucaved. Nice !")
+        }
+        })
+})
+app.get ("/capiatore/gaulois", (req,res)=>{
+    connection.query("SELECT * FROM gaulois",(err,result)=>{
+        if (err){
+            console.error(err);
+            res.status(500).send("Error retrieving gaulois from database")
+        }else{
+            res.json(result)
+        }
+    })
+})
+
 
 app.listen(port,(err)=>{
     if(err){
